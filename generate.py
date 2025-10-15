0180f44c2b0ec2966be02d18f4b52201046060b7
Renat Nasridinov <mavladi@gmail.com>
2025-10-15 20:48:41 +0300

    python3 utf-8

--- generate.py
+++ generate.py
@@ -1,23 +1,19 @@
 #-*- encoding: utf8 -*-
-from StringIO import StringIO
+from io import StringIO
 
 
 def gen_text_file(text):
     text_file = StringIO()
-    text_file.write(text.encode('utf8'))
+    text_file.write(text)
     text_file.seek(0)
     return text_file
 
 
 def gen_result_text(result, code):
     """Generate text for file from checking result and code"""
-    text_list = ["Check results",
-                 "=============",
-                 ""]
+    text_list = ["Check results", "=============", ""]
     text_list.extend(result.split(":::"))
-    text_list.extend(["",
-                      "Code",
-                      "============="])
+    text_list.extend(["", "Code", "============="])
     text_list.append(code)
     return '\n'.join(text_list)
 
