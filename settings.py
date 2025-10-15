3ce69dbf73af2cac364f15f11c870d658d2942aa
Renat Nasridinov <mavladi@gmail.com>
2025-10-15 21:19:04 +0300

    settings skel

--- settings.py
+++ settings.py
@@ -1,5 +1,15 @@
-#-*- encoding: utf8 -*-
+import tempfile
+from urllib.parse import quote_plus
 
-TEMP_PATH = '/tmp/'
+DEBUG = False
 LOG = False
-LOG_FILE = '/var/www/pep8online/logs/app_log'
+LOG_FILE = 'app.log'
+TEMP_PATH = tempfile.gettempdir()
+DB_NAME = 'pep8checker'
+DEBUG = False
+SECRET_KEY = '0199dd51-4ee1-7d68-98f3-7dcbbaaccfb3'
+USER = "user"
+PASSWORD = quote_plus("password")
+HOST = "127.0.0.1"
+
+POSTGRES_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:5432/{DB_NAME}?gssencmode=disable"
